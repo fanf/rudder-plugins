2b3ce972c2cc0e3574516f8f7dad71d579f6d38e
ElaadF <elaad.f@gmail.com>
2020-06-03 10:23:04 +0200

    Fixes #17205: Notifiy change validation request by email

--- change-validation/src/main/scala/com/normation/plugins/changevalidation/NotificationService.scala
+++ change-validation/src/main/scala/com/normation/plugins/changevalidation/NotificationService.scala
@@ -1,35 +1,36 @@
 package com.normation.plugins.changevalidation
 
 import java.io.File
+import java.io.FileReader
+import java.io.StringWriter
 import java.util.Properties
 
 import bootstrap.liftweb.FileSystemResource
 import bootstrap.liftweb.RudderConfig
+import com.github.mustachejava.DefaultMustacheFactory
 import com.normation.NamedZioLogger
 import com.normation.errors.IOResult
+import com.normation.errors.Inconsistency
+import com.normation.errors._
+import com.normation.eventlog.EventLog
 import com.normation.plugins.changevalidation.TwoValidationStepsWorkflowServiceImpl.Deployment
 import com.normation.plugins.changevalidation.TwoValidationStepsWorkflowServiceImpl.Validation
+import com.normation.rudder.domain.eventlog.AddChangeRequest
+import com.normation.rudder.domain.eventlog.DeleteChangeRequest
+import com.normation.rudder.domain.eventlog.ModifyChangeRequest
+import com.normation.rudder.domain.workflows.ChangeRequest
 import com.normation.rudder.domain.workflows.WorkflowNode
+import com.normation.rudder.services.eventlog.ChangeRequestEventLogService
 import com.typesafe.config.Config
 import com.typesafe.config.ConfigFactory
 import javax.mail.Session
 import javax.mail._
 import javax.mail.internet.InternetAddress
 import javax.mail.internet.MimeMessage
-import zio.ZIO
 import zio.syntax._
-import com.github.mustachejava
-import com.github.mustachejava.DefaultMustacheFactory
-import com.normation.eventlog.EventLog
-import com.normation.rudder.domain.eventlog.AddChangeRequest
-import com.normation.rudder.domain.eventlog.DeleteChangeRequest
-import com.normation.rudder.domain.eventlog.ModifyChangeRequest
-import com.normation.rudder.domain.workflows.ChangeRequest
-import com.normation.rudder.domain.workflows.ChangeRequestId
-import com.normation.rudder.services.eventlog.ChangeRequestEventLogService
-import com.normation.rudder.web.components.DateFormaterService
-import net.liftweb.common.EmptyBox
-import net.liftweb.common.Full
+
+import scala.jdk.CollectionConverters._
+
 
 case class Email(value: String)
 
@@ -44,7 +45,7 @@ case class SMTPConf(
 )
 
 case class EmailConf(
-  state     : WorkflowNode
+    state   : WorkflowNode
   , to      : Set[Email]
   , replyTo : Set[Email]
   , cc      : Set[Email]
@@ -56,56 +57,42 @@ case class EmailConf(
 
 class NotificationService(
   changeRequestEventLogService: ChangeRequestEventLogService
-  , path: Option[String]
 ) {
-  val logger          = NamedZioLogger("plugin.change-validation")
+  val configMailPath = "/opt/rudder/etc/plugins/change-validation.conf"
+  val logger         = NamedZioLogger("plugin.change-validation")
 
-  def sendNotification(text: String, step: WorkflowNode, cr: ChangeRequest): ZIO[Any, Nothing, Object] = {
-    path match {
-      case Some(p) =>
-        val notif = for {
-          serverConfig <- getSMTPConf(p)
-          emailConf <- getStepMailConf(step, p)
-          emailBody <- getContentFromTemplate(emailConf)
-          sendingMail <- sendEmail(serverConfig, emailBody, emailConf)
-        } yield (sendingMail)
-
-        notif.catchAll {
-          err =>
-            for {
-              _ <- logger.error(s"Sending `${step.id.value}` notification have failed, cause by : ${err}")
-            } yield {
-              step
-            }
-        }
-      case None =>
-        logger.warn(s"No notification will be sent, cause by : missing email path in configuration.")
-        step.succeed
-    }
+  def sendNotification(step: WorkflowNode, cr: ChangeRequest): IOResult[Unit] = {
+    for {
+      serverConfig <- getSMTPConf(configMailPath)
+      emailConf    <- getStepMailConf(step, configMailPath)
+      params       <- extractChangeRequestInfo(cr)
+      emailBody    <- getContentFromTemplate(emailConf, params)
+      _            <- sendEmail(serverConfig, emailBody, emailConf)
+    } yield ()
   }
 
-  private[this] def sendEmail(conf: SMTPConf, content: String, mailParameter: EmailConf) = {
+  private[this] def sendEmail(conf: SMTPConf, emailBody: String, mailParameter: EmailConf): IOResult[Unit] = {
 
     val prop = new Properties()
     prop.put("mail.smtp.host", conf.smtpHostServer)
     prop.put("mail.smtp.port", conf.port)
     prop.put("mail.smtp.starttls.enable", "true")
 
-    val session = (conf.login, conf.password) match {
-      case (Some(l), Some(p)) =>
-        prop.put("mail.smtp.auth", "true");
-        val auth = new Authenticator() {
-          override protected def getPasswordAuthentication = new PasswordAuthentication(l.value, p)
-        }
-        Session.getInstance(prop, auth)
-      case (_, None)          =>
-        prop.put("mail.smtp.auth", "false");
-        Session.getInstance(prop, null)
-      case (None, _)          =>
-        prop.put("mail.smtp.auth", "false");
-        Session.getInstance(prop, null)
-    }
-    try {
+    IOResult.effect {
+      val session = (conf.login, conf.password) match {
+        case (Some(l), Some(p)) =>
+          prop.put("mail.smtp.auth", "true");
+          val auth = new Authenticator() {
+            override protected def getPasswordAuthentication = new PasswordAuthentication(l.value, p)
+          }
+          Session.getInstance(prop, auth)
+        case (_, None)          =>
+          prop.put("mail.smtp.auth", "false");
+          Session.getInstance(prop, null)
+        case (None, _)          =>
+          prop.put("mail.smtp.auth", "false");
+          Session.getInstance(prop, null)
+      }
       val message = new MimeMessage(session);
       message.setFrom(new InternetAddress(conf.email.value))
       message.setRecipients(
@@ -118,116 +105,114 @@ class NotificationService(
       message.setReplyTo(reply)
       message.setSubject(mailParameter.subject);
 
-      message.setText(content);
+      message.setContent(emailBody, "text/html; charset=utf-8");
       Transport.send(message)
-      mailParameter.succeed
-    } catch {
-      case e: MessagingException =>
-        logger.error(s"Sending mail to notification have failed, cause by : ${e.getMessage}").fail
     }
   }
 
-  private[this] def getConfig(path: String): Config = {
-    val file           = new File(path)
-    val configResource = if (file.exists && file.canRead) {
-      Some(FileSystemResource(file))
-    } else {
-      logger.error("Can not find configuration file specified by JVM property %s: %s ; abort")
-      throw new Exception("Configuration file not found: %s".format(file.getPath))
+  private[this] def getConfig(path: String): IOResult[Config] = {
+    val file = new File(path)
+
+    IOResult.effectM {
+      for {
+        configResource <- if (file.exists && file.canRead) {
+          FileSystemResource(file).succeed
+        } else {
+          Inconsistency(s"Configuration file not found: ${file.getPath}").fail
+        }
+      } yield {
+        ConfigFactory.load(ConfigFactory.parseFile(configResource.file))
+      }
     }
-    ConfigFactory.load(ConfigFactory.parseFile(configResource.value.file))
   }
 
   private[this] def getSMTPConf(path: String): IOResult[SMTPConf] = {
-    IOResult.effect(s"An error occurs while parsing SMTP conf in ${path}") {
-      val config     = getConfig(path)
-      val hostServer = config.getString("smtp.hostServer")
-      val port       = config.getInt("smtp.port")
-      val email      = config.getString("smtp.email")
-      val login      = {
-        val l = config.getString("smtp.login")
-        if (l.isEmpty) None else Some(Username(l))
+    for {
+      config <- getConfig(path)
+      smtp   <- IOResult.effect(s"An error occurs while parsing SMTP conf in ${path}") {
+        val hostServer = config.getString("smtp.hostServer")
+        val port       = config.getInt("smtp.port")
+        val email      = config.getString("smtp.email")
+        val login      = {
+          val l = config.getString("smtp.login")
+          if (l.isEmpty) None else Some(Username(l))
+        }
+        val password   = {
+          val p = config.getString("smtp.password")
+          if (p.isEmpty) None else Some(p)
+        }
+        SMTPConf(
+          hostServer
+          , port
+          , Email(email)
+          , login
+          , password
+        )
       }
-      val password   = {
-        val p = config.getString("smtp.password")
-        if (p.isEmpty) None else Some(p)
-      }
-      SMTPConf(
-        hostServer
-        , port
-        , Email(email)
-        , login
-        , password
-      )
-    }
+    } yield smtp
   }
 
   private[this] def getStepMailConf(step: WorkflowNode, path: String): IOResult[EmailConf] = {
-    IOResult.effect(s"Error when getting `${step.id}` email configuration") {
-      val s = step match {
-        case Validation => "validation"
-        case Deployment => "deployment"
-        case _ => "unsupported"
-      }
-
-      val config = getConfig(path)
-      val to = config.getString(s"${s}.to").split(",").map(Email).toSet
-      val replyTo = config.getString(s"${s}.replyTo").split(",").map(Email).toSet
-      val cc = config.getString(s"${s}.cc").split(",").map(Email).toSet
-      val bcc = config.getString(s"${s}.bcc").split(",").map(Email).toSet
-      val subject = config.getString(s"${s}.subject")
-      val template = config.getString(s"${s}.template")
-      EmailConf(
-        Validation
-        , to
-        , replyTo
-        , cc
-        , bcc
-        , subject
-        , template
-      )
+    val s = step match {
+      case Validation => "validation"
+      case Deployment => "deployment"
+      case _ => "unsupported"
     }
+    for {
+      config   <- getConfig(path)
+      envelope <- IOResult.effect{
+                    val to       = config.getString(s"${s}.to").split(",").map(Email).toSet
+                    val replyTo  = config.getString(s"${s}.replyTo").split(",").map(Email).toSet
+                    val cc       = config.getString(s"${s}.cc").split(",").map(Email).toSet
+                    val bcc      = config.getString(s"${s}.bcc").split(",").map(Email).toSet
+                    val subject  = config.getString(s"${s}.subject")
+                    val template = config.getString(s"${s}.template")
+                    EmailConf(
+                      Validation
+                      , to
+                      , replyTo
+                      , cc
+                      , bcc
+                      , subject
+                      , template
+                    )
+                 }
+    } yield envelope
   }
 
-  private[this] def getContentFromTemplate(emailConf: EmailConf): IOResult[String] = {
-    import com.github.mustachejava.DefaultMustacheFactory
-    import com.github.mustachejava.Mustache
-    import com.github.mustachejava.MustacheFactory
-    import java.io.PrintWriter
+  private[this] def getContentFromTemplate(emailConf: EmailConf, param: Map[String, String]): IOResult[String] = {
     IOResult.effect(s"Error when getting `${emailConf.template}` template configuration"){
-      import java.io.StringWriter
-      val writer = new StringWriter
-      val mf     = new DefaultMustacheFactory
-      val mustache = mf.compile(emailConf.template)
-      val m = Map(
-        "toto" -> "tutu"
-      )
-      mustache.execute(writer, m).flush()
-      writer.toString
+      val writer   = new StringWriter()
+      val mf       = new DefaultMustacheFactory
+      val mustache = mf.compile(new FileReader(emailConf.template), emailConf.template)
+      mustache.execute(writer, param.asJava).toString
     }
   }
 
-  private[this] def extractChangeRequestInfo(cr: ChangeRequest) = {
-    changeRequestEventLogService.getLastLog(cr.id) match {
-      case eb:EmptyBox =>
-        "Error when retrieving the last action".fail
-      case Full(None)  => "Error, no action were recorded for that change request".fail //should not happen here !
-      case Full(Some(e:EventLog)) =>
-        val actionName = e match {
-          case _: ModifyChangeRequest => "Modified"
-          case _: AddChangeRequest    => "Created"
-          case _: DeleteChangeRequest => "Deleted"
-        }
-        Map(
-            "title" -> s"CR #${cr.id}: ${cr.info.name}"
-          , "description" -> cr.info.description
-          , "actionName" -> actionName
-          , "author" -> cr.owner
-          , "date" -> e.creationDate.toString
-          , "link" -> "toto"
-          , "diff" -> "titi"
-        ).succeed
-//        (s"${actionName} on ${DateFormaterService.getDisplayDate(e.creationDate)} by ${e.principal.name}",Some(e.creationDate))
+  private[this] def extractChangeRequestInfo(cr: ChangeRequest): IOResult[Map[String, String]] = {
+    for {
+      lastLog  <- changeRequestEventLogService.getLastLog(cr.id).toIO
+      eventLog <- lastLog match {
+                    case None           =>
+                      Inconsistency("Error when retrieving the last action").fail
+                    case Some(e: EventLog) => e.succeed
+                  }
+      actionName = eventLog match {
+                     case _: ModifyChangeRequest => "Modified"
+                     case _: AddChangeRequest    => "Created"
+                     case _: DeleteChangeRequest => "Deleted"
+                   }
+      date       =  eventLog.creationDate.toDate.toString
+    } yield {
+      Map(
+          "id"          -> cr.id.value.toString
+        , "info"        -> cr.info.name
+        , "description" -> cr.info.description
+        , "actionName"  -> actionName
+        , "author"      -> cr.owner
+        , "date"        -> eventLog.creationDate.toDate.toString
+        , "link"        -> RudderConfig.linkUtil.changeRequestLink(cr.id)
+      )
     }
   }
 }
